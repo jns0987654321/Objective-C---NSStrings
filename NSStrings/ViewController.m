e728c938577bf6082a88cbc0bacf2002f1fb2e3c
Junior Samaroo <jns0987654321@gmail.com>
2017-04-29 18:23:21 -0400

    Learnt Strings :)

--- NSStrings/ViewController.m
+++ NSStrings/ViewController.m
@@ -16,7 +16,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *firstName = @"John"; // string literals
+    NSString *allocatedString = [[NSString alloc]init]; // init for classes/objects
+    
+    NSLog(@"Print me!");
+    NSLog(firstName);
+    NSLog(@"Name: %@", firstName); //proper way % is special char for placement and @ for a string
+    
+    NSString *fullName = [NSString stringWithFormat:@"%@ Smith %@", firstName, @"Charles"];
+    
+    NSString *display = [fullName stringByAppendingString:@" - President from 2222"];
+    
+    NSLog(@"Fullname: %@", fullName);
+    NSLog(@"%@", display);
+    
+    NSString *var1 = @"Junk";
+    NSString *var2 = @"junk";
+    
+    // String Comparison
+    if ([var1 isEqualToString:var2])
+    {
+        NSLog(@"They are the same 1");
+    }
+    if ([var1.lowercaseString isEqualToString:var2])
+    {
+        NSLog(@"They are the same 2");
+    }
+    if ([var1 caseInsensitiveCompare:var2] == NSOrderedSame)
+    {
+        NSLog(@"They are the same 3");
+    }
+    
+    
 }
 
 
